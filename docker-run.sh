c2f168b5b19df928d2c46bee722bef1166c73116
Yuan Shao <shaoyuanqmk@gmail.com>
2022-08-05 14:46:05 +0800

    Update 优化Docker运行命令

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,5 @@
 docker stop informal_bot > /dev/null 2>&1
 docker rm informal_bot > /dev/null 2>&1
 image_tag=(`cat version | grep "version" -m 1 | cut -d= -f2 | sed 's/\"//g'`)
-docker run -d -p 8199:8199 --name="informal_bot"  informal_bot:"v${image_tag}"
+docker run -d -p 8199:8199 --name="informal_bot" -v /var/log/informal:/var/log/informal -v /app/dailycare_for_feishu_bot/config:/var/www/informal/config informal_bot:"v${image_tag}"
 docker logs -f informal_bot
